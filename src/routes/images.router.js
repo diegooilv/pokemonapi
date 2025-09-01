cce3ba0e260535b145effb4c1773ee4071db146a
DiegoOliv07 <diego.oliv2007@gmail.com>
2025-08-31 23:29:16 -0300

    feat(router): serve /images with Cross-Origin-Resource-Policy header

--- src/routes/images.router.js
+++ src/routes/images.router.js
@@ -1,9 +1,13 @@
 import { Router } from "express";
-import express from 'express';
-import path from 'path';
+import express from "express";
+import path from "path";
 
 const router = Router();
 
-router.use('/', express.static(path.resolve('./images')));
+router.use("/", (req, res, next) => {
+  res.setHeader("Cross-Origin-Resource-Policy", "cross-origin");
+  next();
+});
+router.use("/", express.static(path.resolve("./images")));
 
 export default router;
